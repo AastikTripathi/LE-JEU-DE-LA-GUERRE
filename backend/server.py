870c43d82317a649a9fa40b948bceee4b57f7a0d
Aastik Tripathi <41700195+AastikTripathi@users.noreply.github.com>
2026-07-09 21:24:35 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -175,5 +175,13 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str):
             del rooms[room_id]  # Clean up memory if empty
 
 
+import os
+
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        "server:app",
+        host="0.0.0.0",
+        port=port
+    )
